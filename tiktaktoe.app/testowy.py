3d6d8340fb0dd1ce7e3b01c068e89982feea8ca2
Slawek Tomczyk <slawomir.tomczyk95@gmail.com>
2022-03-13 13:15:34 +0100

    testy funkcjonaliności funkcji

--- tiktaktoe.app/testowy.py
+++ tiktaktoe.app/testowy.py
@@ -4,5 +4,36 @@ def list_to_string(list):
     return a
 
 
-row_1 = ["_","_","_"]
+def win_column():
+    if (row_1[0] == row_2[0] == row_3[0] and row_1[0] =="X") or (row_1[0] == row_2[0] == row_3[0] and row_1[0] == "O"):
+        print("Wygrałeś 1")
+        return True       
+    elif (row_1[1] == row_2[1] == row_3[1] and row_1[1] =="X") or (row_1[1] == row_2[1] == row_3[1] and row_1[1] == "O"):
+        print("Wygrałeś 2")
+        return True
+    elif (row_1[2] == row_2[2] == row_3[2] and row_1[2] =="X") or (row_1[2] == row_2[2] == row_3[2] and row_1[2] == "O"):
+        print("Wygrałeś 3")
+        return True 
+
+def win_cross():
+    if row_1[0] == row_2[1] == row_3[2] and row_1[0] == "X" or row_1[0] == row_2[1] == row_3[2] and row_1[0] == "O":
+        print("Wygrałeś 4")
+        return True
+    elif row_1[2] == row_2[1] == row_3[0] and row_1[2] == "X" or row_1[2] == row_2[1] == row_3[0] and row_1[2] == "O":
+        print("Wygrałeś 5")
+        return True
+
+
+row_1 = ["X","_","O"] # row_1[0] == row_1[1] == row_1[2] 
+row_2 = ["_","O","_"]
+row_3 = ["O","_","_"]
 print(list_to_string(row_1))
+
+
+if row_1[0] == row_1[1] == row_1[2] and "_" in row_1:
+    print("True")
+
+
+win_column()
+win_cross()
+
